d5eea267d8963eba6ef00627b2fa03630d57a253
Partha <parthodas638@gmail.com>
2022-05-24 10:50:50 +0600

    Initial Commit

---
+++ VedioPlayerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VedioPlayerDemo
+//
+//  Created by Annanovas IT on 24/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
